ec029869dd1af7739c4c144c69b5dc19778a1add
d3lonkun <118652855+d3lonkun@users.noreply.github.com>
2022-12-23 17:44:48 +0800

    Delete server.js
    Ada

--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require(express)();
-
-app.get('/', (req, res) => res.send(`Connected to the server, and the bot must be ready right now.`));
-
-module.exports = () => {
-  app.listen(3000);
-}
